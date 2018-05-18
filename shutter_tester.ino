735291633a4507bdbc4be09ca6747039e067563f
Ondrej Pok <opok@users.noreply.github.com>
2018-05-18 13:36:49 +0200

    Update shutter_tester.ino

--- shutter_tester.ino
+++ shutter_tester.ino
@@ -1,28 +1,34 @@
 /*
- * circuit:
- *  5V -------------+
- *                  |
- *             phototransistor
- *                  |
- * digital pin -----+
- *                  |
- *                 10kOhm
- *                  |
- *                 gnd
+ * @author Ondrej Pok
+ * @version 2.0
+ *
+ * circuit uses Arduino Nano and its internal pullup resistor.
+ * phototransistor is connected between D2 and GND, and second between D3 and GND.
+ * 
+ * Digital pin is set to HIGH, but when light hits phototransistor, 
+ * that pulls the volatage of the pin to the ground so reading of the pin returns LOW.
+ * I use interrupts to capture the exact moment the pin is pulled low or goes high.
+ *
+ * The output on LCD shows shutter speed as well as curtain travel time from sensor to sensor.
+ * My sensors are 30x20mm apart, positioned diagonally for measuring horizontal as well as vertical shutter,
+ * so the travel time is multiplied by 1.2 to give result for 36mm or 24mm travel.
+ * 
+ * uses https://github.com/JChristensen/Timer v2 library
+ * https://github.com/todbot/SoftI2CMaster for LCD via I2C
  */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <Wire.h>
 #include <LCD.h>
-// uses NewLiquidCrystal library
+#include <Timer.h>
 #include <LiquidCrystal_I2C.h>
 
-// input digital pins
 #define SENSOR_1 2
 #define SENSOR_2 3
 
-#define I2C_ADDR    0x27 // I2C address of LCD
-#define BACKLIGHT_PIN     3
+#define I2C_ADDR       0x3f
+#define BACKLIGHT_PIN  3
 #define En_pin  2
 #define Rw_pin  1
 #define Rs_pin  0
@@ -33,74 +39,30 @@
 
 #define ONE_SECOND 1000000
 
-/* Turn on backlight for set period of time
- * disable measuring during this time
- * but checkTime must be called externally.
- */
-class LcdBacklight {
-  private:
-    unsigned long startTime;
-    unsigned long period;
-    LiquidCrystal_I2C* lcd;
-    boolean lcdOn;
-
-  public:
-    LcdBacklight() {}
-    
-    void begin(LiquidCrystal_I2C* lcd, unsigned long seconds) {
-      Serial.println("init lcd backlight");
-      this->period = seconds;
-      this->lcd = lcd;
-      this->lcd->setBacklight(HIGH);
-      delay(500);
-      this->lcd->setBacklight(LOW);
-      this->lcdOn = false;      
-    }
-  
-    boolean start() {
-      if(this->lcdOn == true) {
-        return false;
-      }
-      Serial.println("lcd backlight turn on");
-      this->startTime = millis();
-      this->lcd->setBacklight(HIGH);
-      delay(200);
-      this->lcdOn = true;
-      return true;
-    }
-  
-    boolean checkTime() {
-      if(this->lcdOn == false) {
-        return false;
-      }
-      if(millis() > this->startTime + this->period * 1000) {
-        Serial.println("lcd backlight turn off");
-        this->lcd->setBacklight(LOW);
-        delay(100);
-        this->lcdOn = false;
-        return true;
-      }
-      return false;
-    }
-};
-
 
 LiquidCrystal_I2C lcd (I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 
-// sensor data arrays, index 0 is when the value went HIGH, index 1 is when it went LOW
+// data storage
+// 1st item is when sensor was uncovered and light started hitting the phototransistor
+// 2nd item is when sensor was covered again
 volatile unsigned long s1[2] = {0, 0}, s2[2] = {0, 0};
-volatile bool dataReady = false;
-LcdBacklight lcdBacklight;
 
-void resetMeasuredData() {
-  dataReady = false;
+volatile bool dataReady = false;
+
+Timer timer;
+
+
+void resetMeasuredData(void *context) {
+  Serial.println("resetting data...");
   s1[0] = 0;
   s1[1] = 0;
   s2[0] = 0;
   s2[1] = 0;
+  dataReady = false;
+  initInterrupts();
 }
 
-void printResult(int row, unsigned long exposure, unsigned long travel) {
+void printResults(int row, unsigned long exposure, unsigned long travel) {
   lcd.setCursor(0, row);
   lcd.print("                ");
   lcd.setCursor(0, row);
@@ -110,13 +72,13 @@ void printResult(int row, unsigned long exposure, unsigned long travel) {
     lcd.print(exposure/1000, DEC); lcd.print("ms"); 
   }
 
-  
+  travel = (unsigned long) 1.2 * travel;
   int whole = travel/1000; // whole part from microseconds to miliseconds
   int fraction = (travel - (travel/1000) * 1000)/10; // 2 fractional digits
-  lcd.setCursor(10 - floor(log10(whole)), row); // log10 -> to tell how many digits the whole part has
+  lcd.setCursor(10 - floor(log10(whole)), row);
   lcd.print(whole, DEC);
   lcd.print('.');
-  if(fraction < 10) lcd.print('0'); // leading zero
+  if(fraction < 10) lcd.print('0');
   lcd.print(fraction, DEC);
   lcd.print("ms");
 }
@@ -129,69 +91,83 @@ void onDataReady() {
   travel1 = max(s1[0], s2[0]) - min(s1[0], s2[0]);
   travel2 = max(s2[1], s1[1]) - min(s2[1], s1[1]);
 
-  printResult(0, exposure1, travel1);
-  printResult(1, exposure2, travel2);
+  Serial.print("Exposure 1 "); Serial.println(exposure1);
+  Serial.print("Exposure 2 "); Serial.println(exposure2);
+  Serial.print("Travel 1 "); Serial.println(travel1);
+  Serial.print("Travel 2 "); Serial.println(travel2);
+  
+  printResults(0, exposure1, travel1);
+  printResults(1, exposure2, travel2);
 
-  lcdBacklight.start();
-}
-
-void resetPendingInterrupts() {
-  EIFR = 0x03; // reset interrupt register, not sure if this works
-}
-
-void s1_up() {
-  resetMeasuredData();
-  s1[0] = micros();
-  Serial.println("s1_up");
-  detachInterrupt(digitalPinToInterrupt(SENSOR_1));
-  attachInterrupt(digitalPinToInterrupt(SENSOR_1), s1_down, FALLING);
+  timer.after(1000, resetMeasuredData, (void*)0);
 }
 
 void s1_down() {
-  s1[1] = micros();
+  if(dataReady) { return; }
+  if(s1[0] == 0){
+    s1[0] = micros();
+  }
   detachInterrupt(digitalPinToInterrupt(SENSOR_1));
+  attachInterrupt(digitalPinToInterrupt(SENSOR_1), s1_up, RISING);
+  Serial.print("Sensor 1 on at "); Serial.println(s1[0]);
 }
 
-void s2_up() {
-  s2[0] = micros();
-  detachInterrupt(digitalPinToInterrupt(SENSOR_2));
-  attachInterrupt(digitalPinToInterrupt(SENSOR_2), s2_down, FALLING);
+void s1_up() {
+  if(dataReady) { return; }
+  if(s1[1] == 0)
+    s1[1] = micros();
+  detachInterrupt(digitalPinToInterrupt(SENSOR_1));
+  Serial.print("Sensor 1 off at "); Serial.println(s1[1]);
 }
 
 void s2_down() {
-  s2[1] = micros();
-  Serial.println("s2_down");
+  if(dataReady) { return; }
+  if(s2[0] == 0)
+    s2[0] = micros();
   detachInterrupt(digitalPinToInterrupt(SENSOR_2));
-  dataReady = true; // after measuring last value, update displayed data on LCD
+  attachInterrupt(digitalPinToInterrupt(SENSOR_2), s2_up, RISING);
+  Serial.print("Sensor 2 on at "); Serial.println(s2[0]);
+}
+
+void s2_up() {
+  if(dataReady) { return; }
+  if(s2[1] == 0)
+    s2[1] = micros();
+  detachInterrupt(digitalPinToInterrupt(SENSOR_2));
+  Serial.print("Sensor 2 off at "); Serial.println(s2[1]);
 }
 
 void initInterrupts() {
-  Serial.println("initInterrupts");
-  resetPendingInterrupts();
-  attachInterrupt(digitalPinToInterrupt(SENSOR_1), s1_up, RISING);  
-  attachInterrupt(digitalPinToInterrupt(SENSOR_2), s2_up, RISING);
+  detachInterrupt(digitalPinToInterrupt(SENSOR_1));
+  detachInterrupt(digitalPinToInterrupt(SENSOR_2)); 
+  EIFR = 0x03; // reset interrupt register
+  attachInterrupt(digitalPinToInterrupt(SENSOR_1), s1_down, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SENSOR_2), s2_down, FALLING);
 }
+
+
 
 void setup()
 {
-  lcd.begin(16,2);
-  lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
-  lcdBacklight.begin(&lcd, 10);
-
-  initInterrupts();
-  
   Serial.begin(9600);
+  Serial.println("setting up...");
+  
+  lcd.begin(16,4);
+  lcd.setBacklightPin(BACKLIGHT_PIN,NEGATIVE);
+
+  // set input pins HIGH
+  digitalWrite(SENSOR_1, HIGH);
+  digitalWrite(SENSOR_2, HIGH);
+  
+  initInterrupts();
 }
 
 void loop()
 {
-  if(lcdBacklight.checkTime()) { // when checkTime returns true, reenable measuring
-    Serial.println("loop");
-    initInterrupts();
-  }
-  if(dataReady == true) {
+  timer.update();
+  
+  if(!dataReady && s1[0] != 0 && s1[1] != 0 && s2[0] != 0 && s2[1] != 0) {
+    dataReady = true;
     onDataReady();
-    dataReady = false;
   }
 }
-
