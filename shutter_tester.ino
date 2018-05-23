c25ab747cac1916b40259bc67d3ba29473640879
Ondrej Pok <opok@users.noreply.github.com>
2018-05-23 17:15:05 +0200

    revert back the file description comment

--- shutter_tester.ino
+++ shutter_tester.ino
@@ -1,7 +1,22 @@
 /*
- * obvod s pouzitim interneho pullup rezistora.
- * fototranzistor je zapojeny medzi digital pin 2/3 a GND
+ * @author Ondrej Pok
+ * @version 2.1
+ *
+ * circuit uses Arduino Nano and its internal pullup resistor.
+ * phototransistor is connected between D2 and GND, and second between D3 and GND.
+ * 
+ * Digital pin is set to HIGH, but when light hits phototransistor, 
+ * that pulls the volatage of the pin to the ground so reading of the pin returns LOW.
+ * I use interrupts to capture the exact moment the pin is pulled low or goes high.
+ *
+ * The output on LCD shows shutter speed as well as curtain travel time from sensor to sensor.
+ * My sensors are 30x20mm apart, positioned diagonally for measuring horizontal as well as vertical shutter,
+ * so the travel time is multiplied by 1.2 to give result for 36mm or 24mm travel.
+ * 
+ * uses https://github.com/JChristensen/Timer v2 library
+ * https://github.com/todbot/SoftI2CMaster for LCD via I2C
  */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <Wire.h>
